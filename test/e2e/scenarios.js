91a6a1a32523dd9e10f5e7cc0bdc43b133081ce4
a.pihtovnikov <a.pihtovnikov@xsolla.com>
2016-06-24 21:01:45 +0500

    Пример e2e теста

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,21 +1,19 @@
 'use strict';
 
 describe('ngs', function() {
-    it('demo', function() {
+    it('Должна происходить автоматическая переадресация на страницу /index, когда адрес пустой', function() {
         browser.get('index.html');
-        expect(browser.getLocationAbsUrl()).toMatch("/index");
+
+        expect(browser.getLocationAbsUrl()).toMatch("/home");
     });
 
-    describe('index', function() {
-
+    describe('home', function() {
         beforeEach(function() {
-            browser.get('index.html/index');
+            browser.get('index.html#!/home');
         });
 
-
-        it('should ...', function() {
-            expect(element.all(by.css('div')).first().getText()).toMatch(/demo/);
+        it('Должна должна отрендериться страница home, когда в адресной строке /home', function() {
+            expect(element.all(by.css('.page')).count()).toEqual(1);
         });
-
     });
 });
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,22 +1,38 @@
 exports.config = {
     allScriptsTimeout: 11000,
+
     specs: [
         './e2e/**/*.js'
     ],
 
-    seleniumServerJar: '../node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.0.jar',
-
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    directConnect: true,
+    chromeDriver: '../node_modules/webdriver-manager/selenium/chromedriver_2.21',
     capabilities: {
-        'browserName': 'phantomjs',
-        'phantomjs.binary.path': './node_modules/phantomjs-prebuilt/bin/phantomjs'
+        browserName: 'chrome'
     },
 
     baseUrl: 'http://localhost:3000/',
 
-    framework: 'jasmine',
+    framework: 'jasmine2',
 
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 30000
+        defaultTimeoutInterval: 30000,
+        isVerbose: true,
+        includeStackTrace: true,
+
+        // Убираем стандартный вывод, если используем репортер
+        print: function() {
+        }
+    },
+
+    // Для наглядности будем использовать репортер
+    onPrepare: function() {
+        var SpecReporter = require('jasmine-spec-reporter');
+
+        jasmine.getEnv().addReporter(new SpecReporter({
+            displayStacktrace: 'all'
+        }));
     }
 };
